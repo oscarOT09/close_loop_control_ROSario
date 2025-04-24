ce9f878b172e66d98f4b8ac4c537712e411df70d
oscar_ot09 <oscar_ot09@outlook.es>
2025-04-24 15:48:36 -0600

    Codigos finales

--- close_loop_control_ROSario/close_loop_control_ROSario/controller_ROSario.py
+++ close_loop_control_ROSario/close_loop_control_ROSario/controller_ROSario.py
@@ -1,4 +1,4 @@
-# Nodo de control | Etapa 2 - Challenge 2
+# Nodo de control | Challenge 3
 
 # Importaciones necesarias
 import rclpy
@@ -9,14 +9,23 @@ import transforms3d
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from rclpy.qos import qos_profile_sensor_data
-#from rcl_interfaces.msg import SetParametersResult
 from rosario_path.msg import RosarioPath
 
 # Definición de la clase
 class OpenLoopCtrl(Node):
     def __init__(self):
         super().__init__('close_loop_ctrl')
-        self.factor = 1.15
+        
+        self.factor = 1.05
+
+        self.factor_dist = 1.09
+        self.factor_ang = 0.8
+        
+        self.max_lin_vel = 0.0
+        self.min_lin_vel = 0.0
+        self.max_ang_vel = 0.0
+        self.min_ang_vel = 0.0
+
         # Posiciones objetivo
         self.X_ref = 0.0
         self.Y_ref = 0.0
@@ -59,11 +68,6 @@ class OpenLoopCtrl(Node):
         # Cola de almacenamiento de mensajes RosarioPath
         self.path_queue = []
 
-        # Inicialización de posiciones
-        #self.prev_pose = np.array([0.0, 0.0])
-        #self.curr_pose = np.array([0.0, 0.0])
-        #self.next_pose = np.array([0.0, 0.0])
-
         # Publicador para el tópico /cmd_vel (comunicación con el Puzzlebot)
         self.cmd_vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
 
@@ -86,7 +90,7 @@ class OpenLoopCtrl(Node):
         # Estado
         self.state = 0  # 0: rotación, 1: movimiento lineal, 2: Reposo
         
-        # Frecuencia de muestreo
+        # Muestreo
         frecuencia_controlador = 200.0
         self.dt_pid = 1.0/200.0
         self.controller_timer = self.create_timer(1.0 / frecuencia_controlador, self.control_loop)
@@ -98,22 +102,7 @@ class OpenLoopCtrl(Node):
         if result < 0:
             result += 2 * np.pi
         return result - np.pi
-    '''
-    def delta_angle(self, prev, current, next_):
-        prev = np.array(prev)
-        current = np.array(current)
-        next_ = np.array(next_)
-
-        v1 = current - prev
-        v2 = next_ - current
-
-        angle1 = np.arctan2(v1[1], v1[0])
-        angle2 = np.arctan2(v2[1], v2[0])
-
-        delta = angle2 - angle1
-        delta = (delta + np.pi) % (2 * np.pi) - np.pi  # Normalización del ágnulo entre -pi y pi
-        return delta  # en radianes
-    '''
+    
     def pid_controller_angular(self, error):
         self.integral_ang += error * self.dt_pid
         derivative = (error - self.prev_error_ang) / self.dt_pid
@@ -139,8 +128,10 @@ class OpenLoopCtrl(Node):
     def listener_callback(self, msg):
         # Descomposición para almacenamiento local en variables 
         new_point = np.array([msg.path.position.x, msg.path.position.y])
-        velocity = msg.velocity
-        duration = msg.time
+        self.max_lin_vel = msg.max_lin_vel
+        self.min_lin_vel = msg.min_lin_vel
+        self.max_ang_vel = msg.max_ang_vel
+        self.min_ang_vel = msg.min_ang_vel
 
         # Guardado en quque
         self.path_queue.append((new_point))
@@ -153,18 +144,21 @@ class OpenLoopCtrl(Node):
         q = msg.pose.pose.orientation
         quaternion = [q.w, q.x, q.y, q.z]
         _, _, yaw = transforms3d.euler.quat2euler(quaternion)
-        self.Th_robot = self.wrap_to_Pi(yaw) #* self.factor
+        self.Th_robot = self.wrap_to_Pi(yaw*self.factor) 
 
     def control_loop(self):
         # Si el robot no está en movimiento y hay elementos faltantes en la queue, se realizan los calculos necesarios para efectuar la siguiente trayectoria 
         if not self.robot_busy and self.path_queue:
             self.X_ref, self.Y_ref = self.path_queue.pop(0)
             
-            dx = self.X_ref - self.X_robot
-            dy = self.Y_ref - self.Y_robot
-            self.ang_ref = np.arctan2(dy, dx)
+            dx = self.X_ref - self.prev_goal_X
+            dy = self.Y_ref - self.prev_goal_Y
+            self.ang_ref = np.arctan2(dy, dx) * self.factor_ang
 
             self.dist_ref = np.hypot(dx, dy)
+
+            self.prev_goal_X = self.X_robot
+            self.prev_goal_Y = self.Y_robot
 
             self.robot_busy = True
             self.state = 0
@@ -177,21 +171,17 @@ class OpenLoopCtrl(Node):
 
             if self.state == 0:
                 
-                #self.get_logger().info(f"Ang_robot: {np.rad2deg(self.Th_robot)} | Ang obj: {np.rad2deg(self.ang_ref)}")
-                
                 error = self.wrap_to_Pi(self.ang_ref - self.Th_robot)
                 
                 self.get_logger().info(f"Error angular: {self.ang_ref}-{self.Th_robot}={np.rad2deg(error):.2f}°")
 
                 if abs(error) < np.deg2rad(10.0):
-                    #self.get_logger().info("LLEGO A LIM. ERROR ANGULAR")
                     self.twist.angular.z = 0.0
                     
                     self.integral_ang = 0.0
                     self.prev_error_ang = 0.0
-
+                    self.factor += 0.125
                     self.state = 1
-                    #self.get_logger().info(f"Estado dese angular: {self.state}.")
                     
                     self.cmd_vel_pub.publish(self.twist)
                     self.get_logger().info("Rotación completada.")
@@ -199,47 +189,39 @@ class OpenLoopCtrl(Node):
                 else:
                     
                     output = self.pid_controller_angular(error)
-                    #self.get_logger().info(f"Salida PID ANGULAR: {output}")
-                    self.twist.angular.z = self.saturate_with_deadband(output, 0.29, 3.0)
-                    #self.get_logger().info(f"V.ANG: {self.twist.angular.z}")
-                    #self.twist.linear.x = 0.0
+                    self.twist.angular.z = self.saturate_with_deadband(output, self.min_ang_vel, self.max_ang_vel)
                     self.cmd_vel_pub.publish(self.twist)
                     return
 
             elif self.state == 1:
                 # TRASLACIÓN
-                self.dist_robot += np.linalg.norm([self.X_robot - self.prev_X_robot, self.Y_robot - self.prev_Y_robot])
-                #error = np.hypot(dx, dy) #error = np.linalg.norm(pow(dx, 2) + pow(dy, 2)) # error = np.sqrt(dx**2 + dy**2) # self.dist_ref - self.dist_robot
+                self.dist_robot += np.linalg.norm([self.X_robot - self.prev_X_robot, self.Y_robot - self.prev_Y_robot]) * self.factor_dist
                 error = self.dist_ref - self.dist_robot
-                
+
                 self.get_logger().info(f"Error lineal: {error:.3f} m")
                 self.prev_X_robot = self.X_robot
                 self.prev_Y_robot = self.Y_robot
 
                 if error < 0.1:
-                    #self.get_logger().info("LLEGO A LIM. ERROR LINEAL")
                     self.twist.linear.x = 0.0
                     self.state = 2
                     self.dist_robot = 0.0
                     self.integral_lin = 0.0
                     self.prev_error_lin = 0.0
 
+                    self.factor_dist += self.dist_robot*0.005
+
                     self.cmd_vel_pub.publish(self.twist)
-                    #self.get_logger().info(f"Estado desde lineal: {self.state}.")
                     self.get_logger().info("Traslación completada.")
                     return
                     
                 else:
                     output = self.pid_controller_lineal(error)
-                    #self.get_logger().info(f"Salida PID LINEAL: {output}")
-                    self.twist.linear.x = self.saturate_with_deadband(output, 0.025, 0.38)
-                    #self.get_logger().info(f"V.LIN: {self.twist.linear.x}")
-                    #self.twist.angular.z = 0.0
+                    self.twist.linear.x = self.saturate_with_deadband(output, self.min_lin_vel, self.max_lin_vel)
                     self.cmd_vel_pub.publish(self.twist)
                     return
 
             elif self.state == 2:
-                #self.twist = Twist()
                 self.twist.linear.x = 0.0
                 self.twist.angular.z = 0.0
 
